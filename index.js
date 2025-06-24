51639a0abd998281b0d936a77d789c4e831750fc
elicett <eliezerlicett@gmail.com>
2025-06-24 02:07:44 -0400

    [PERSONAL EXAMPLE] Part 3, subpart a: Node.js and Express, Excercise 3.1: Backend in the phone book (step 1): Some example GET/POST/DELETE Methods stored

--- index.js
+++ index.js
@@ -25,12 +25,39 @@ let persons = [
     }
 ]
 
-//app.use(express.json())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person=> person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end('That contact doesnt exists')
+    }
+    
+})
+
+app.post('/api/persons', (request, response) => {
+    const persons = request.body
+    console.log(persons)
+    response.json(persons)
+})
+
+app.delete('/api/persons/:id', (request, response)=> {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+    response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
