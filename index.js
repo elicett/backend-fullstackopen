688b674c167c21dd02fe4e34dc8bba2414667b4f
elicett <eliezerlicett@gmail.com>
2025-06-27 15:04:10 -0400

    Part 3, subpart a: Node.js and Express, Excercise 3.7: Backend in the phone book (step 7): Morgan Tiny

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-/*Part 3, subpart a: Node.js and Express, Excercise 3.6: Backend in the phone book (step 6): Conditionals (Missing name or number - The name already exists)-*/
+/*Part 3, subpart a: Node.js and Express, Excercise 3.7: Backend in the phone book (step 6): Conditionals (Missing name or number - The name already exists)-*/
 const express = require('express')
+const morgan = require("morgan")
 const app = express()
 
 let persons = [
@@ -26,13 +27,29 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+/*
+morgan.token('body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+  else if (req.method === 'GET' || req.method === 'DELETE') {
+    return ''
+  }
+  else return 'Invalid Method';
+});
+
+
+app.use(morgan(':method :url :status :response-time ms :body', {}));
+*/
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+    return response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    return response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -40,9 +57,9 @@ app.get('/api/persons/:id', (request, response) => {
     const person = persons.find(person=> person.id === id)
 
     if (person) {
-        response.json(person)
+        return response.json(person)
     } else {
-        response.status(404).end('That contact doesnt exists')
+        return response.status(404).end('That contact doesnt exists')
     }
     
 })
@@ -55,32 +72,34 @@ app.post('/api/persons', (request, response) => {
         number: request.body.number
     }
     if (!newObject.name || !newObject.number){
-        response.json({
+        return response.json({
             Error: 'You should fill both (name and number) fields',
         })
     }
 
     if (persons.find(value => value.name === newObject.name)){
-        response.status(409).json({
+        return response.status(409).json({
             Error: 'Contact already exists'
         })
     }
 
     persons = persons.concat(newObject);
-    console.log(newObject)
-    response.json({
+    //console.log(newObject)
+    return response.json({
         message: 'Contact added',
         contact: newObject,
         entireData: persons
     })
 })
 
+
+
 app.delete('/api/persons/:id', (request, response)=> {
     const id = Number(request.params.id)
     const contact = persons.find(value => value.id === id)
 
     if (!contact){
-        response.status(404).end('This contact doesnt exist')
+        return response.status(404).end('This contact doesnt exist')
     }
 
 
@@ -88,7 +107,7 @@ app.delete('/api/persons/:id', (request, response)=> {
 
 
 
-    response.status(202).json({
+    return response.status(202).json({
         message: `Contact '${contact.name}' deleted correctly`
         })
 })
@@ -97,7 +116,7 @@ app.delete('/api/persons/:id', (request, response)=> {
 app.get('/info', (request, response)=>{
     const lengthPersons = persons.length
     const dateNow = new Date()
-    response.status(200).send(`
+    return response.status(200).send(`
         <div>
             <p>Phonebook has info for ${lengthPersons} persons</p>
             <p>Request received at ${dateNow}</p>
@@ -108,6 +127,14 @@ app.get('/info', (request, response)=>{
 
 
 
+
+
+
+
+const unknownEndpoint = (request, response) => {
+  return response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
